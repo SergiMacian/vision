ed97e06f177bc7c84505996a8f1387fde33b7278
Sergi Macián Ribera <45793287+SergiMacian@users.noreply.github.com>
2020-06-19 15:50:34 +0200

    update transforms for LSP

--- references/detection/transforms.py
+++ references/detection/transforms.py
@@ -13,7 +13,24 @@ def _flip_coco_person_keypoints(kps, width):
     flipped_data[inds] = 0
     return flipped_data
 
+def _flip_lsp_person_keypoints(kps, width):
+    flip_inds = [5, 4, 3, 2, 1, 0, 11, 10, 9, 8, 7, 6, 12, 13]
+    #flip_inds = [0, 2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13, 16, 15]
+    flipped_data = kps[:, flip_inds]
+    flipped_data[..., 0] = width - flipped_data[..., 0]
 
+    #In LSP it seems that either the visibility = 0 means visible or that
+    #it is wrongly tagged. Therefore, we will not use it. 
+
+    # Maintain COCO convention that if visibility == 0, then x, y = 0
+    #inds = flipped_data[..., 2] == 0
+
+    #If using LSP visibility = 0 (visible) and 1 (non-visible):
+    inds = flipped_data[..., 2] == 1
+
+    #flipped_data[inds] = 0
+    return flipped_data
+    
 class Compose(object):
     def __init__(self, transforms):
         self.transforms = transforms
@@ -39,7 +56,7 @@ class RandomHorizontalFlip(object):
                 target["masks"] = target["masks"].flip(-1)
             if "keypoints" in target:
                 keypoints = target["keypoints"]
-                keypoints = _flip_coco_person_keypoints(keypoints, width)
+                keypoints = _flip_lsp_person_keypoints(keypoints, width)
                 target["keypoints"] = keypoints
         return image, target
 
