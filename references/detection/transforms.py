1be4bfd81031b5e829d718f02e4f9f0b572ca3c5
Sergi Macián Ribera <45793287+SergiMacian@users.noreply.github.com>
2020-09-22 00:34:44 +0200

    Update transforms.py
    correcció RandomRotate 2 (el de 180º)

--- references/detection/transforms.py
+++ references/detection/transforms.py
@@ -102,7 +102,7 @@ class RandomRotation(object):
             #Com rota cap a l'esquerra, rotem 2 cops (=270º esquerra = 90º dreta)
             image = torch.rot90(image,2,[1,2])
             boxes = target["boxes"]
-            boxes[:,0],boxes[:,1],boxes[:,2],boxes[:,3] = width-boxes[:,0],height-boxes[:,3],width-boxes[:,2],height-boxes[:,1]
+            boxes[:,0],boxes[:,1],boxes[:,2],boxes[:,3] = width-boxes[:,2],height-boxes[:,3],width-boxes[:,0],height-boxes[:,1]
             target["boxes"] = boxes
             keypoints = target["keypoints"]
             keypoints[...,0], keypoints[...,1] = width-keypoints[...,0],height - keypoints[...,1]
